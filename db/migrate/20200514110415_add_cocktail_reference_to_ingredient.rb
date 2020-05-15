269a9a40f8a75504bf395961e403d046d341fabe
Eloise Tait <eloisetait@yahoo.com>
2020-05-15 12:55:27 +0100

    Deleted migrations

--- db/migrate/20200514110415_add_cocktail_reference_to_ingredient.rb
+++
@@ -1,5 +0,0 @@
-class AddCocktailReferenceToIngredient < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :ingredients, :cocktail, null: false, foreign_key: true
-  end
-end
--- db/migrate/20200514114106_drop_table.rb
+++
@@ -1,6 +0,0 @@
-class DropTable < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :doses
-    drop_table :ingredients
-  end
-end
